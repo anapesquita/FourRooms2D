c8ab206b8c79268fe754ed01ee0d2195da8a9f76
anapesquita <39096629+anapesquita@users.noreply.github.com>
2024-10-04 14:38:06 +0100

    working on Ana's mac

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -30,10 +30,10 @@ public class DataController : MonoBehaviour {
     public bool participantFeedbackSubmitted = false;
     public bool questionAnswered = false;
 
-    // Data file saving
+    //Data file saving
     //AP private string baseFilePath = "/Users/a.pesquita@bham.ac.uk/Documents/GitHub/FourRooms2D/data/";
-    private string baseFilePath = "C:/Users/pesquita/Documents/GitHub/FourRooms2D/data/";
-    //private string baseFilePath = @".\data\";  // for Berlin fMRI machine
+    //private string baseFilePath = "C:/Users/pesquita/Documents/GitHub/FourRooms2D.git/data/";
+    private string baseFilePath = @"\data\";  //for Berlin fMRI machine
     public DateTime dateTime = DateTime.Now;
     public string stringDateTime; 
     public string filePath;
--- Assets/Scripts/filepath.cs
+++ Assets/Scripts/filepath.cs
@@ -4,5 +4,5 @@ using System.Collections;
 public class filepath
 {
    //AP public static string path = "/Users/a.pesquita@bham.ac.uk/Documents/GitHub/FourRooms2D/";
-    public static string path = "C:/Users/pesquita/Documents/GitHub/FourRooms2D/";
+    public static string path = "C:/Users/pesquita/Documents/GitHub/FourRooms2D.git/";
 }
